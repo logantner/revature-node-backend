c3d2c391d88617724d251c59be93e4bb71f6aef7
logantner <logangantner@gmail.com>
2020-06-18 15:53:27 -0400

    added cors credentials

--- src/server.ts
+++ src/server.ts
@@ -20,7 +20,9 @@ const app: Application = express();
 const port = process.env.PORT || 8084;
 
 // console.log(process.env);
-app.use(cors());
+app.use(cors({
+    credentials: true,
+}));
 
 app.use(express.json());
 app.use(express.urlencoded());
