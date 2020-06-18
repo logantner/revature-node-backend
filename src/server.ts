37124be4a70e90b796bee0b1448dd926a3c3ae52
logantner <logangantner@gmail.com>
2020-06-18 16:56:37 -0400

    some more random attempts

--- src/server.ts
+++ src/server.ts
@@ -22,19 +22,38 @@ const app: Application = express();
 const port = process.env.PORT || 8084;
 
 // console.log(process.env);
-app.use(cors({
-    credentials: true,
-}));
+// app.use(cors({
+//     credentials: true,
+// }));
 
 app.use(express.json());
 app.use(express.urlencoded());
 app.use(session({ 
     secret: "mySecret",
-    cookie: { 
-        secure: false, 
-        httpOnly: false
-    } 
+    // cookie: { 
+    //     secure: false, 
+    //     httpOnly: false
+    // } 
 }));
+
+app.use(function (req, res, next) {
+
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', '*');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', "true");
+
+    // Pass to next layer of middleware
+    next();
+});
 
 // app.use(session({
 //     secret: 'secret',
@@ -50,14 +69,14 @@ app.use(session({
 //     unset: 'destroy'
 //   }));
 
-const corsOptions = {
-    origin: /.your.domain.com$/,    // reqexp will match all prefixes
-    methods: "GET,HEAD,POST,PATCH,DELETE,OPTIONS",
-    credentials: true,                // required to pass
-    allowedHeaders: "Content-Type, Authorization, X-Requested-With",
-  }
+// const corsOptions = {
+//     origin: /.your.domain.com$/,    // reqexp will match all prefixes
+//     methods: "GET,HEAD,POST,PATCH,DELETE,OPTIONS",
+//     credentials: true,                // required to pass
+//     allowedHeaders: "Content-Type, Authorization, X-Requested-With",
+//   }
 // intercept pre-flight check for all routes
-app.options('*', cors(corsOptions))
+// app.options('*', cors(corsOptions))
 
 app.use("/auth", authRouter);
 app.use("/search", searchRouter);
